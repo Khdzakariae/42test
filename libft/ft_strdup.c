73c9fae8c3a2846f3a11bfe2ae6594b724e1aafd
Zakariae El-Khadir <zel-khad@c3r7p7.1337.ma>
2023-11-04 15:48:32 +0100

    hello

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,48 @@
 /*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 14:49:02 by zel-khad          #+#    #+#             */
-/*   Updated: 2023/11/04 14:49:03 by zel-khad         ###   ########.fr       */
+/*   Updated: 2023/11/04 15:47:37 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include<stddef.h>
+#include <stdlib.h>
+
+void *ft_calloc(size_t nmemb, size_t size)
+{
+    void *ptr;
+    ptr = malloc(nmemb * size);
+    if (!ptr)
+    {
+        free(ptr);
+        return(0);
+    }
+    return (ptr);
+}
+
+char *ft_strdup(const char *s, size_t n)
+{
+    size_t i;
+    i = 0;
+    
+    char *ptr;
+    
+    ptr = ft_calloc(sizeof(char) , n);
+    
+    while (i < n)
+    {
+        ptr[i] = s[i];
+        i++;
+    }
+    return ((char*)ptr);
+}
+
+
+#include <stdio.h>
+
+int main ()
+{
+    const char  *s;
+    s = "hello";
+    printf("%s",ft_strdup( s, 10));
+}
