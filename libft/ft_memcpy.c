5929c69386c951bca147f385b96a7671b179be24
Zakariae El-Khadir <zel-khad@c3r7p7.1337.ma>
2023-11-04 12:15:11 +0100

    hello

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,13 +6,13 @@
 /*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 12:56:12 by zel-khad          #+#    #+#             */
-/*   Updated: 2023/11/04 11:55:09 by zel-khad         ###   ########.fr       */
+/*   Updated: 2023/11/04 12:12:28 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 #include <stddef.h>
-void *memcpy(void *dest, const void *src, size_t n)
+void *ft_memcpy(void *dest, const void *src, size_t n)
 {
     size_t i ;
     unsigned char *ptr;
@@ -42,7 +42,7 @@ int main ()
 {
     int source[] = {1, 2, 3};
     int destination[20];
-    memcpy(destination, source, 3*4);
+    ft_memcpy(destination, source, 3*4);
     
     for (int i = 0; i < 3; i++)
     {
--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,14 +6,14 @@
 /*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 16:12:13 by zel-khad          #+#    #+#             */
-/*   Updated: 2023/11/04 12:05:27 by zel-khad         ###   ########.fr       */
+/*   Updated: 2023/11/04 12:11:58 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
 
-void *memmve(void *dest, const void *src, size_t n)
+void *cheack(void *dest, const void *src, size_t n)
 {
         size_t i;
         unsigned char *ptr;
@@ -30,7 +30,7 @@ void *memmve(void *dest, const void *src, size_t n)
         ptr[i]= '\0';
         return((void*)dest);
 }
-void *memmove(void *dest, const void *src, size_t n)
+void *ft_memmove(void *dest, const void *src, size_t n)
 {
     unsigned char *ptr;
     unsigned char *p;
@@ -50,7 +50,7 @@ void *memmove(void *dest, const void *src, size_t n)
         }
     }
     else
-        memmve (dest , src, n);
+        cheack(dest , src, n);
     return ((void*)dest);
 }
 #include <stdio.h>
@@ -60,5 +60,5 @@ int main()
     char source[] = "my friend";
     printf("%s\n", source);
     printf("Expected string: myriend\n");
-    printf("%s\n", (char *)memmove(source + 2, source, 2));
+    printf("%s\n", (char *)ft_memmove(source + 2, source, 2));
 }
