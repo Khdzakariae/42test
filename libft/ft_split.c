824ae1a8f44319ad7309af5b099a54c34380a72d
Zakariae El-Khadir <zel-khad@c4r3p6.1337.ma>
2023-11-07 11:25:13 +0100

    hello

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 16:16:42 by zel-khad          #+#    #+#             */
-/*   Updated: 2023/11/07 09:16:39 by zel-khad         ###   ########.fr       */
+/*   Updated: 2023/11/07 11:09:50 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,116 +15,114 @@
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
-    void *ptr;
-    ptr = malloc(nmemb * size);
-    if (!ptr)
-        return (NULL);
-    return (ptr);
+	void	*ptr;
+
+	ptr = malloc(nmemb * size);
+	if (!ptr)
+		return (NULL);
+	return (ptr);
 }
 
-char *ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-    char *ptr = ft_calloc(len + 1, sizeof(char));
-    if (ptr == NULL)
-    {
-        return NULL;
-    }
+	char			*ptr;
+	unsigned int	i;
 
-    unsigned int i = 0;
-
-    while (s[start + i] != '\0' && i < len)
-    {
-        ptr[i] = s[start + i];
-        i++;
-    }
-
-    ptr[i] = '\0';
-
-    return ptr;
+	i = 0;
+	ptr = ft_calloc(len + 1, sizeof(char));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	while (s[start + i] != '\0' && i < len)
+	{
+		ptr[i] = s[start + i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
 }
 
-int checkwords(char const *s, char c)
+int	checkwords(char const *s, char c)
 {
-    size_t i;
-    int words;
+	size_t	i;
+	int		words;
 
-    i = 0;
-    words = 0;
-
-    while (s[i])
-    {
-        if (s[i] != c)
-        {
-            while (s[i] && s[i] != c)
-            {
-                i++;
-            }
-            words++;
-        }
-        i++;
-    }
-    return words;
+	i = 0;
+	words = 0;
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			while (s[i] && s[i] != c)
+			{
+				i++;
+			}
+			words++;
+		}
+		i++;
+	}
+	return (words);
 }
 
-char **zaki(char const *s, char c, char **ptr, int j)
+char	**zaki(char const *s, char c, char **ptr, int j)
 {
-    int start;
-    int end;
-    int i;
+	int	start;
+	int	end;
+	int	i;
 
-    start = 0;
-    end = 0;
-    i = 0;
-
-    while (s[i] != '\0')
-    {
-        if (s[i] != c)
-        {
-            start = i;
-            while (s[i] != '\0' && s[i] != c)
-                i++;
-            end = i;
-            ptr[j] = ft_substr(s, start, end - start);
-            j++;
-        }
-        else
-            i++;
-    }
-    return ptr;
+	start = 0;
+	end = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] != c)
+		{
+			start = i;
+			while (s[i] != '\0' && s[i] != c)
+				i++;
+			end = i;
+			ptr[j] = ft_substr(s, start, end - start);
+			j++;
+		}
+		else
+			i++;
+	}
+	return (ptr);
 }
 
-char **ft_split(char const *s, char c)
+char	**ft_split(char const *s, char c)
 {
-    int smp;
-    int j = 0;
-    char **ptr;
+	int		j;
+	int		smp;
+	char	**ptr;
 
-    smp = checkwords(s, c);
-    ptr = ft_calloc(smp + 1, sizeof(char *));
-    ptr = zaki(s, c, ptr, j);
-
-    if (ptr[j] == NULL)
-    {
-        while (j >= 0)
-        {
-            free(ptr[j]);
-            j--;
-        }
-        free(ptr);
-        return NULL;
-    }
-    return ptr;
+	j = 0;
+	smp = checkwords(s, c);
+	ptr = ft_calloc(smp + 1, sizeof(char *));
+	ptr = zaki(s, c, ptr, j);
+	if (ptr[j] == NULL)
+	{
+		while (j >= 0)
+		{
+			free(ptr[j]);
+			j--;
+		}
+		free(ptr);
+		return (NULL);
+	}
+	return (ptr);
 }
-
+/*
 int main()
 {
-    char const *s = " gav ejlh giejbk kgeg jm";
-    char **result = ft_split(s, ' ');
+	char const *s = " gav ejlh giejbk kgeg jm";
+	char **result = ft_split(s, ' ');
 
-    for (int j = 0; result[j] != NULL; j++)
-    {
-        printf("%s\n", result[j]);
-        free(result[j]);
-    }
-    return 0;
-}
+	for (int j = 0; result[j] != NULL; j++)
+	{
+		printf("%s\n", result[j]);
+		free(result[j]);
+	}
+	return 0;
+}*/
